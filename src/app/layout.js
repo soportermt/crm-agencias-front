93447e88429403cf5a77fcaa20ce77b87e3bb409
MizaelUB <mizaelmizrrainub@gmail.com>
2026-06-20 10:32:45 -0600

    bootstrap config

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,8 @@
 import { Geist, Geist_Mono } from "next/font/google";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
 import "./globals.css";
+import BootstrapClient from "@/components/BootstrapClient";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,6 +23,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <BootstrapClient />
         {children}
       </body>
     </html>
